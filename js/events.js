d7352a54ee7af2d85f47de37ba9846faf5636458
Erin Green <erin.y.green@gmail.com>
2018-08-01 20:25:36 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -20,9 +20,8 @@ function pressIt() {
 
 function submitIt() {
   $("form").on("submit", function() {
-if ($( "input:first" ).val() === "correct") {
     alert("Your form is going to be submitted now.");
-    return;}
+    return;
 });
 }
 
