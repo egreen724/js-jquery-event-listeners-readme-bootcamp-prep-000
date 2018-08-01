334de63002883a94a650ada9159e0ce94ea085dc
Erin Green <erin.y.green@gmail.com>
2018-08-01 20:19:10 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -1,22 +1,23 @@
 $('p').on("click", function getIt() {
     alert('Hey!');
-  }
+  });
+
 
 $('.tasty').on("load", function frameIt() {
   
-}
+});
 
 $("#typing").on("keydown", function pressIt(key) {
   if (key.which == 71) {
     alert("G was pressed"); 
   }
-}
+});
 
 $('form').on("submit", function submitIt() {
 if ($( "input:first" ).val() === "correct") {
     alert('Your form is going to be submitted now.');
-    return;
-}
+    return;}
+});
 
 
 $(document).ready(function(){
