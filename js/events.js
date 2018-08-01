44315fffbed9fffa4da013f4201719b90d3aee62
Erin Green <erin.y.green@gmail.com>
2018-08-01 20:01:15 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -1,4 +1,18 @@
-//define functions here
+function getIt() {
+  $('p').on(click, )
+}
+
+function frameIt() {
+  
+}
+
+function pressIt() {
+  
+}
+
+function submitIt() {
+  
+}
 
 $(document).ready(function(){
 
